e2a7b81c0dbcdd6c7d22032d9bfb389d3f501f0e
Marinavazmed <marina.vazquez.medinilla.al@iespoligonosur.org>
2023-01-29 19:15:21 +0100

    models vehiculos

--- main/models.py
+++ main/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Vehiculo(models.Model):
+    TIPOS = [
+        ('Coche', 'Coche'),
+        ('Ciclomotor', 'Ciclomotor'),
+        ('Motocicleta', 'Motocicleta'),
+    ]
+
+    MARCAS = [
+        ('Renault', 'Renault'),
+        ('Seat', 'Seat'),
+        ('BMW', 'BMW'),
+    ]
+
+    COLORES = [
+        ('Negro', 'Negro'),
+        ('Rojo', 'Rojo'),
+        ('Gris', 'Gris'),
+        ('Azul', 'Azul'),
+
+    ]
+    matricula = models.CharField(primary_key=True, max_length=30)
+    tipo = models.CharField(max_length=11, choices=TIPOS, default='Coche')
+    chasis = models.PositiveIntegerField(unique=True)
+    marca = models.CharField(max_length=30, choices=MARCAS, default='BMW')
+    modelo = models.CharField(max_length=40)
+    color = models.CharField(max_length=30, choices=COLORES, default='Negro')
+    fecha_fabrica = models.DateField()
+    fecha_matricula = models.DateField(null=True, blank=True)
+    fecha_baja = models.DateField(null=True, blank=True)
+    suspendido = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.matricula + ": " + self.marca + ", " + self.modelo + ", " + self.color
