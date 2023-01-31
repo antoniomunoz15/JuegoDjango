4eb256521e84edad6cb65466706e44e8cb876511
Marinavazmed <marina.vazquez.medinilla.al@iespoligonosur.org>
2023-01-31 20:19:24 +0100

    fin ej 1

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 from main import views
 
@@ -15,6 +16,15 @@ urlpatterns = [
     path('', include(router.urls)),
     path('i18n/', include('django.conf.urls.i18n')),
     path('', TemplateView.as_view(template_name='main/index.html'), name='welcome'),
+    path('openapi', get_schema_view(
+        title="Your Project",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='main/swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
 ]
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
